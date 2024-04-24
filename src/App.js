8c010337a4d5f6d17a3cbc2bc1d69ff8f48c8123
shubham gupta <45961394+shubham7633@users.noreply.github.com>
2024-04-24 22:28:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,20 +5,6 @@ import { DarkModeContext } from "./DarkModeContext.js";
 export default function App() {
   const { isDark, toogleDarkMode } = useContext(DarkModeContext);
 
-  Array.prototype.customMyFilter = (callback) => {
-    let results = [];
-    for (let i = 0; i < this.length; i++) {
-      if (callback(this[i], i, this)) {
-        results.push(this[i]);
-      }
-    }
-    return results;
-  };
-
-  let spaceArr = [1, 2, 3, 4, 5];
-
-  let result = spaceArr.customMyFilter((elem) => elem > 4);
-
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
